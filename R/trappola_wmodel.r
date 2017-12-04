e006b7e0390936e6d62c76a97a8b4a9624647661
Alfonso <alfcrisci@gmail.com>
2017-12-04 14:41:10 +0100

    Delete trappola_wmodel.r

--- R/trappola_wmodel.r
+++
@@ -1,7 +0,0 @@
-#' trappola_wmodel
-#'
-#' @description Multivariate linear model  of daily mean water temperature observed in a mosquito REDLAV trap in Italy based on  daily minimum and maximum air temperature and length of day.
-#' @format A lm R object.
-#' 
-#' @source Istituto di Biometeorologia Firenze Italy  Alfonso crisci \email{a.crisci@@ibimet.cnr.it} 
-"trappola_wmodel"
